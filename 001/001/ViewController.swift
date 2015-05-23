3c04f07408b719a32d9319cc5da3cd1a0ed877ed
Dark10CRK <lirenzhao@gmail.com>
2015-05-24 00:09:41 +0800

    create project code D001

---
+++ 001/001/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  001
+//
+//  Created by Dark10CRK on 15/5/24.
+//  Copyright (c) 2015年 Dark10CRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("view did load")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
