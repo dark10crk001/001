bfc53dd240e605834b2054d3b7f17da5afb27c35
Dark10CRK <lirenzhao@gmail.com>
2015-05-24 09:13:30 +0800

    do nothing

--- 001/001/ViewController.swift
+++ 001/001/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        NSLog("did recieve memory warning")
     }
 
 
